063f5bd54f47ca7a59144664b29c52d110d0b811
Matt Strange <mstrange86@gmail.com>
2014-10-16 11:56:45 -0700

    copy scripts

--- copy_binaries_debug.bat
+++ copy_binaries_debug.bat
@@ -1,3 +1,7 @@
+echo.
+echo.
+echo COPYING COMPILED FILES TO MEDIAMONKEY 
+
 xcopy /e /y %~dp0\vis_milk2\output\Winamp\plugins\*     c:\bin\MediaMonkey\Plugins\
 xcopy    /y %~dp0\vis_milk2\Debug\vis_milk2.dll         c:\bin\MediaMonkey\Plugins\
 
