b87c2964b1a71c3211b602beedb7db2a09aa77fd
Matt Strange <mstrange86@gmail.com>
2014-11-30 13:18:52 -0800

    compile standard library statically to remove dependency on MSVCR120.DLL

--- copy_binaries_release.bat
+++ copy_binaries_release.bat
@@ -2,7 +2,7 @@ echo.
 echo.
 echo COPYING COMPILED FILES TO MEDIAMONKEY 
 
-xcopy /e /y  %~dp0\vis_milk2\output\Winamp\plugins\*   c:\bin\MediaMonkey\Plugins\
-xcopy    /y  %~dp0\vis_milk2\Release\vis_milk2.dll     c:\bin\MediaMonkey\Plugins\
+xcopy /e /y  %~dp0\vis_milk2\output\Winamp\plugins\*   %appdata%\MediaMonkey\Plugins\
+xcopy    /y  %~dp0\vis_milk2\Release\vis_milk2.dll     %appdata%\MediaMonkey\Plugins\
 
 
